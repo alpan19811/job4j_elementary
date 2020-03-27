d241f6ab52b0ef1ec8e47c593cf3f29dfa6a2a55
Aleksei <alpan19811@gmail.com>
2020-03-27 15:26:37 +0300

    3.4. Заголовок в файле java[#262003]

---
+++ src/ru/job4j/calculator/Calculator.java
@@ -0,0 +1,19 @@
+package ru.job4j.calculator;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int one = 1;
+        int two = 2;
+        int onePlastwo = one + two;
+        System.out.println(onePlastwo);
+        int six = 6;
+        int four = 4;
+        int five = 5;
+        int sixDivtwo = six / two;
+        int fiveMinestwo = five - two;
+        int fourTimetwo = four * two;
+        System.out.println(sixDivtwo);
+        System.out.println(fiveMinestwo);
+        System.out.println(fourTimetwo);
+    }
+}
